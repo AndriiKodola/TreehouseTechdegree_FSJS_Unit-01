e5683350f6f7459f492519c954971d37eff26ee4
AndriiKodola <ankodola@gmail.com>
2018-12-01 23:28:45 +0100

    Have written all necessary code for the project

--- js/script.js
+++ js/script.js
@@ -6,46 +6,59 @@ FSJS project 1 - A Random Quote Generator
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
+//An array of quotes with 'quote', 'sorce', and optional 'year' proberty is being created.
+const quotes = [
+  {
+    quote: "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
+    source: "Dr. Suess",
+    year: "2013"
+  },
+  {
+    quote: "Life is what happens when you’re busy making other plans.",
+    source: "John Lennon",
+    year: "1957"
+  },
+  {
+    quote: "When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.",
+    source: "Helen Keller",
+    year: "1929"
+  },
+  {
+    quote: "A successful man is one who can lay a firm foundation with the bricks others have thrown at him.",
+    source: "Chinmayananda Saraswati",
+  },
+  {
+    quote: "The only impossible journey is the one you never begin.",
+    source: "Anthony Robbins"
+  }
+];
 
-  Recommended: 
-    - Add at least one `year` and/or `citation` property to at least one 
-      quote object.
-***/
+//Function returns a random quote from quotes array
+const getRandomQuote = () => {
+  let randomNumber = Math.floor(Math.random()*quotes.length);
+  return quotes[randomNumber];
+};
 
+//Function inserts properly formatter HTML into index.html (random quote + existing prop).
+const printQuote = () => {
+  let randomQuote = getRandomQuote();
 
+  let stringHTML = `
+    <p class="quote">${randomQuote.quote}</p>
+    <p class="source">${randomQuote.source}
+  `;
+  if (randomQuote.hasOwnProperty('year')) { //Check for prop existance
+    stringHTML += `<span class="year">${randomQuote.year}</span>`;
+  }
+  stringHTML += '</p>';
 
+  document.getElementById('quote-box').innerHTML = stringHTML;
+};
 
 /***
-  Create the `getRandomQuote` function to:
-   - generate a random number 
-   - use the random number to `return` a random quote object from the 
-     `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - call the `getRandomQuote` function and assign it to a variable.
-   - use the properties of the quote object stored in the variable to 
-     create your HTML string.
-   - use conditionals to make sure the optional properties exist before 
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
+  When the "Show another quote" button is clicked, the event listener
+  below will be triggered, and it will call, or "invoke", the `printQuote`
+  function. So do not make any changes to the line of code below this
   comment.
 ***/
 
