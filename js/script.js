7217f59bbc3f7a243997a1132e8772983c3c7715
AndriiKodola <ankodola@gmail.com>
2018-12-02 13:10:16 +0100

    Added "Exceeds Expectations" content

--- js/script.js
+++ js/script.js
@@ -7,29 +7,35 @@ FSJS project 1 - A Random Quote Generator
 
 
 //An array of quotes with 'quote', 'sorce', and optional 'year' proberty is being created.
+//Exceeds Expectations: Objects in array include additional properties, such as categorization tags.
 const quotes = [
   {
     quote: "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
     source: "Dr. Suess",
-    year: "2013"
+    year: "2013",
+    positivity: "positive"
   },
   {
     quote: "Life is what happens when you’re busy making other plans.",
     source: "John Lennon",
-    year: "1957"
+    year: "1957",
+    positivity: "neutral"
   },
   {
     quote: "When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.",
     source: "Helen Keller",
-    year: "1929"
+    year: "1929",
+    positivity: "neutral"
   },
   {
     quote: "A successful man is one who can lay a firm foundation with the bricks others have thrown at him.",
     source: "Chinmayananda Saraswati",
+    positivity: "positive"
   },
   {
     quote: "The only impossible journey is the one you never begin.",
-    source: "Anthony Robbins"
+    source: "Anthony Robbins",
+    positivity: "neutral"
   }
 ];
 
@@ -37,6 +43,18 @@ const quotes = [
 const getRandomQuote = () => {
   let randomNumber = Math.floor(Math.random()*quotes.length);
   return quotes[randomNumber];
+};
+
+//Exceeds Expectations: Quotes change automatically after a certain amount of time passes. Background color changes each time the quote changes.
+//Random color generator.
+const setRandomBackgroudColor = () => {
+  let randomColorHex = Math.floor(Math.random()*16777215).toString(16);
+  if (randomColorHex > 'f8f8f8') {
+    document.querySelector('body').style.color = '##00c500';
+  }
+
+  document.querySelector('body').style.textShadow = '2px 2px #000';
+  document.querySelector('body').style.backgroundColor = `#${randomColorHex}`;
 };
 
 //Function inserts properly formatter HTML into index.html (random quote + existing prop).
@@ -50,10 +68,14 @@ const printQuote = () => {
   if (randomQuote.hasOwnProperty('year')) { //Check for prop existance
     stringHTML += `<span class="year">${randomQuote.year}</span>`;
   }
-  stringHTML += '</p>';
+  stringHTML += `<span class="positivity">${randomQuote.positivity}</span></p>`;
 
+  setRandomBackgroudColor();
   document.getElementById('quote-box').innerHTML = stringHTML;
 };
+
+//Executes printQuote every 3 sec
+setInterval(printQuote, 3000);
 
 /***
   When the "Show another quote" button is clicked, the event listener
